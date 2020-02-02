b7e30babe3df4b2a7aa1275883de151cefc5983f
Felipevbc <60027336+Felipevbc@users.noreply.github.com>
2020-02-01 21:50:49 -0500

    Create Application.java

---
+++ Project 1 - src/Application.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/* In this project, my goal is to create a program that will encrypt a four digit integer from the user, by
+ *first prompting for the input and then subsenquently sending the input to Encryption, where the program will
+ * add a value of 7 to it and then divide it by 10. Also, in the same program, the user should be able to reverse
+ *this process, by multiplying the number given by 10 and then adding 7.
+ * 
+ */
+ 
+ public class Application {
+ 
+ }
