be4da8f92164957fc5126d6e385ec4ec93bc3be4
Felipevbc <60027336+Felipevbc@users.noreply.github.com>
2020-02-01 22:45:54 -0500

    Update Application.java

--- Project 1 - src/Application.java
+++ Project 1 - src/Application.java
@@ -1,12 +1,32 @@
 import java.util.Scanner;
 
-/* In this project, my goal is to create a program that will encrypt a four digit integer from the user, by
- *first prompting for the input and then subsenquently sending the input to Encryption, where the program will
- * add a value of 7 to it and then divide it by 10. Also, in the same program, the user should be able to reverse
- *this process, by multiplying the number given by 10 and then adding 7.
- * 
- */
- 
- public class Application {
- 
- }
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+  try (Scanner integer = new Scanner(System.in)) {
+		int n;
+			
+			System.out.println("Select Method: 1. Encryption or 2. Decryption");
+			int choice = integer.nextInt();
+			
+			if (choice == 1 ) {
+		
+			System.out.println("\nEnter a four digit number:");	
+			n = integer.nextInt();
+			
+	
+			System.out.println("\nThe encrypted number is: " +Encrypter.encrypt(n));
+			}
+			
+			if (choice == 2 ) {
+				
+				System.out.println("\nEnter a four digit number:");	
+				n = integer.nextInt();
+				
+				System.out.println("\nThe decrypted number is: " +Decrypter.decrypt(n));
+			}
+		}
+	}
+}
