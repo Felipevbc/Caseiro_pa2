b9be89aaffbf6487e3ab53bf4b75fd5642626637
Felipevbc <60027336+Felipevbc@users.noreply.github.com>
2020-02-02 00:51:08 -0500

    Update BMICalculator.java

--- Project 2 - src/BMICalculator.java
+++ Project 2 - src/BMICalculator.java
@@ -1,99 +1,98 @@
 import java.util.*;
 
-// This program calculates BMI using a given Unit Type
+
 public class BMICalculator {
 	
     public static void main(String[] args) {
-    	
-    // Initialize Scanner
-        Scanner console = new Scanner(System.in);
+    	Scanner console = new Scanner(System.in);
         
-    // Prompt User for the preferred Unit Type
-        System.out.println("Select Unit Type: 1. Metric or 2. Imperial");
+    // Prompt the user for a unit system
+        System.out.println("Please select the preferred unit system: 1. Metric  2. Imperial\n");
         
-    // Store the selected choice
+    
         int choice = console.nextInt();
         
         
-        // Run this function if the user selects Metric Units
+    //Metric system option
         if(choice == 1) {
         	
-        // Prompt user for metric Height and Weight and store the input
-            System.out.println("\nEnter Height in Centimeters: ");
-            double cm = console.nextInt();   
-            
-        // Convert Centimeters to Meters
-            double height = cm/100;
-            
+    //Prompt the user for the height
+            System.out.println("Enter Height in Centimeters: ");
+            double centimeters = console.nextInt();   
+    //Convert centimeters into meters
+	    double height = centimeters/100;
+    //Prompt the user for weight        
             System.out.println("\nEnter Weight in Kilograms: ");
             double weight = console.nextDouble();
             
-        // Calculate the BMI using the Metric formula 
-            double bmi = (weight / (height * height));
-           
-        // Print out calculated BMI
+    //Calculate the bmi    
+            double bmi = (weight / (height*2));
+          
+       
             if (bmi >= 0) {
-            System.out.printf("\nBMI = %.1f\n",bmi);
+            System.out.printf("Your BMI is %.1f\n",bmi);
             } else {
-            	System.out.printf("\nInvalid Entry... Please Try Again");
+            	System.out.printf("\nInvalid Entry");
             }
             
-            // Determine the BMI category and print the result
+            
             if(bmi >= 0) {
             	if (bmi < 18.5) {
-            		System.out.println("\nCategory: Underweight");
+            		System.out.println("You are underweight");
                 
             	} else if (bmi < 25) {
-            		System.out.println("\nCategory: Normal");
+            		System.out.println("You are at normal weight");
                 
             	} else if (bmi < 30) {
-            		System.out.println("\nCategory: Overweight");
+            		System.out.println("You are overweight");
                 
             	} else {
-            		System.out.println("\nCategory: Obese");               
+            		System.out.println("You are a Obese");               
             }
         }
      }
             	
-        // Run this function if user selects Imperial Units    
+          
         if(choice == 2) {
         	
-        // Prompt user for the imperial Height and Weight and store the input
+        
         	System.out.println("\nEnter Height in Feet: ");
             double impFeet = console.nextInt();
         	
             System.out.println("\nEnter additional Height in Inches: ");
         	double impHeight = console.nextDouble();
         	
-        // Convert Feet to Inches and add additional height if included
+        
         	impHeight = impFeet*12 + impHeight;
         
         	System.out.println("\nEnter Weight in Pounds: ");     
         	double impWeight = console.nextDouble();
         	
-        // Calculate the BMI using the Imperial formula
+        
         	double impBmi = (impWeight * 703 / impHeight / impHeight);
         
-        // Print out the calculated BMI
+        
         	   if (impBmi >= 0) {
                    System.out.printf("\nBMI = %.1f\n",impBmi);
-                   } else {
-                   	System.out.printf("\nInvalid Entry... Please Try Again");
+                   } 
+		
+		else {
+                   	System.out.printf("Invalid Entry");
                    }
         
-        	// Determine the BMI Category and print the result
+        	
         	  if(impBmi >= 0) {
         		if (impBmi < 18.5) {
-        			System.out.println("\nCategory: Underweight");
+        			System.out.println("You are underweight");
             
         		} else if (impBmi < 25) {
-        			System.out.println("\nCategory: Normal");
+        			System.out.println("You are at a normal weight");
             
         		} else if (impBmi < 30) {
-        			System.out.println("\nCategory: Overweight");
+        			System.out.println("You are overweight");
             
         		} else {
-        			System.out.println("\nCategory: Obese");  
+        			System.out.println("You are obese");  
         		}
         	}
         }
